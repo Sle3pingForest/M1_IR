d8b7a60f4480375abda7175587f5d7271018abdc
Sle3pingForest <huenam.ly@gmail.com>
2019-04-09 14:19:48 +0200

    upadte nam parti

--- Nam/testBis.py
+++ Nam/testBis.py
@@ -27,7 +27,7 @@ def coord2(s,f):
 
 # assemble x,y coordinates and sticker colors into three arrays
 def donnees(data):
-    x, y, color, color_true, group_color = [], [], [], [], []
+    x, y, color, color_true, group_color,tab_inter = [], [], [], [], [],[]
     max_reading = 0
     
     tab_new_rgb_centre=[]
@@ -43,17 +43,24 @@ def donnees(data):
             x.append(tx)
             y.append(ty)
             color.append([round(r)/round(max_reading), round(g)/round(max_reading), round(b)/round(max_reading)])
+            tab_inter.append([r,g,b])
+            #color.append([round(r)/255, round(g)/255, round(b)/255])
             if f == 4:
+                rateRed = (round(r)/round(max_reading)) * 100
+                rateGreen =(round(g)/round(max_reading)) * 100
+                rateBlue = (round(b)/round(max_reading)) * 100
+                print 'Taux rouge: ', rateRed , ' - Taux Vert: ', rateGreen, ' - Taux Bleu: ', rateBlue
                 color_true.append([r, g, b])
-                new_r = round(r/(r+g+b),3)
-                new_g = round(g/(r+g+b),3)
-                new_b = round(b/(r+g+b),3)
+                new_r = round(rateRed/(rateRed+rateGreen+rateBlue),3)
+                new_g = round(rateGreen/(rateRed+rateGreen+rateBlue),3)
+                new_b = round(rateBlue/(rateRed+rateGreen+rateBlue),3)
+                print '% rouge: ', new_r , ' - % Vert: ', new_g, ' - % Bleu: ', new_b
         tab_new_rgb_centre.append([new_r,new_g,new_b])
-    return x,y,color, tab_new_rgb_centre, color_true, max_reading
+    return x,y,color, tab_new_rgb_centre, color_true, max_reading,tab_inter
 
 # draw the unfolding
 def draw(data):
-    x, y, color, tab_new_rgb_centre, c, max_color = donnees(data)
+    x, y, color, tab_new_rgb_centre, c, max_reading,tab_inter = donnees(data)
    
     n = 9
     m = 6
@@ -74,63 +81,49 @@ def draw(data):
             indice = 0
             size = len(c)
         	  
-           #v = '#{:02x}{:02x}{:02x}'.format( 200, 200 , 254 )
-           #min_color = int(v[1:], 16)
                 
             cfR =round(r/(r+g+b),3)
             cfG =round(g/(r+g+b),3)
             cfB =round(b/(r+g+b),3)
             tab_coeff.append([cfR,cfG,cfB])
-                #color_center[indice].append( [r,g,b] )
     
-
-	  #print indice
-         
-    #tab_coeff = sorted(tab_coeff.iteritems(), key=lambda (k,v): (v,k))
-    #print tab_coeff
-    #print "\n"
-    #print len(tab_coeff)
-    #print "\n"
-    #print tab_new_rgb_centre
-  
-    #print "color center taille"
-    #print len(color_center[0])
+    print tab_new_rgb_centre
+    print '\n'
+    for i in range(0,6):
+        for j in range(0,9):
+            print tab_coeff[i*9+j]
+        print '\n'
+        
     for i in range(0,54):
-        var = int(i/9)
-        m = i%9
-        ecart = 2500000
-        ok = False;
+        var = i/9
         for j in range(0,len(tab_new_rgb_centre)):
             difR = abs(tab_coeff[i][0]-tab_new_rgb_centre[j][0])
             difG = abs(tab_coeff[i][1]-tab_new_rgb_centre[j][1])
             difB = abs(tab_coeff[i][2]-tab_new_rgb_centre[j][2])
-            if(difR + difG + difB < ecart ):
-                ecart = difR+difG+difB
+            if(difR < 0.1 and difG < 0.1 and difB < 0.1):
                 var = j
-                ok = True
-        if(len(color_center[var]) < 9):
-            color_center[var].append((data[var][m]))
+        color_center[int(var)].append(tab_inter[i])
 
-
+            
     test_coeff= color_center
     tamere = []
     tonpere = []
     for i in range (0, len(c)):
-        tamere.append( [round(c[i][0])/68,  round(c[i][1]) / 68, round(c[i][2])/68] )
+        tamere.append( [round(c[i][0])/max_reading,  round(c[i][1]) / max_reading, round(c[i][2])/max_reading] )
          
 
     k = 0;
     for i in range(0,len(c)):
-        print "\n"
         plt.scatter(i, 0, s=800, marker='s', c=tamere[i])
         p = 5
         for j in range(0, len(test_coeff[i]) ):
+            print test_coeff[i][j]
             yplacer = j+10
             p +=10
             tonpere.append([round(test_coeff[i][j][0]/68,3), round(test_coeff[i][j][1]/68,3), round(test_coeff[i][j][2]/68,3)])
             plt.scatter(i, p+yplacer , s=800, marker='o', c=tonpere[k])
             k = k +1
-    
+        print '\n'
     plt.savefig('unfolding.png')
     plt.show()
     
@@ -151,6 +144,10 @@ def comparaisonStat(data1, data2):
         print data2[i]
     return compt
 
+
+
+
+
 data = [
 [[9, 11, 8],[28, 38, 21],[37, 55, 26],[30, 40, 22],[13, 20, 11],[35, 47, 25],[44, 64, 32],[33, 47, 24],[44, 65, 32],
 ],[[1, 2, 4],[2, 4, 6],[3, 6, 5],[2, 5, 6],[1, 2, 3],[2, 3, 5],[5, 9, 7],[2, 3, 6],[1, 3, 4],
@@ -168,6 +165,14 @@ data_turned =[
 ],[[35, 52, 26],[36, 54, 27],[37, 53, 25],[38, 12, 4],[29, 7, 2],[36, 11, 3],[38, 12, 4],[35, 13, 4],[37, 13, 4],
 ],[[33, 11, 3],[33, 12, 3],[39, 13, 6],[44, 56, 47],[41, 50, 48],[46, 60, 50],[52, 66, 54],[41, 55, 45],[46, 61, 50],
 ],]
+"""[[4, 4, 8],[4, 5, 8],[4, 6, 8],[3, 5, 7],[5, 7, 9],[3, 6, 7],[3, 5, 8],[4, 5, 7],[4, 5, 8]],
+[[27, 39, 44],[30, 44, 45],[26, 38, 43],[46, 52, 37],[59, 68, 45],[47, 52, 37],[22, 9, 4],[26, 10, 5],[24, 8, 5]],
+[[39, 46, 35],[29, 10, 5],[32, 37, 45],[47, 54, 38],[37, 19, 7],[36, 43, 46],[32, 34, 28],[29, 10, 4],[30, 33, 43]],
+[[27, 39, 45],[31, 45, 46],[28, 39, 44],[38, 43, 47],[52, 59, 62],[38, 43, 50],[22, 8, 4],[26, 9, 4],[20, 7, 4]],
+[[38, 44, 35],[35, 48, 50],[27, 32, 43],[48, 54, 39],[47, 64, 62],[36, 43, 48],[41, 46, 37],[35, 48, 48],[30, 35, 44]],
+[[11, 29, 32],[13, 32, 34],[9, 25, 29],[12, 33, 33],[16, 42, 41],[10, 33, 34],[11, 30, 32],[13, 33, 34],[10, 28, 30]],
+]
+"""
 
 #draw(data)
 draw(data_turned)
